f47b713733cdceba2d17581c1ca0b9f760d76dc4
JeraldCrisBergantinos <44352027+JeraldCrisBergantinos@users.noreply.github.com>
2023-04-27 16:09:54 +0800

    Updated main.lua
    Updated some comments.

--- pong/main.lua
+++ pong/main.lua
@@ -2,9 +2,11 @@ WINDOW_WIDTH = 1280
 WINDOW_HEIGHT = 720
 
 --[[
-    Runs when the game first starts up, only once, used to initialize the game
+    Runs when the game first starts up, only once, used to initialize the game.
 ]]
 function love.load()
+    -- use nearest-neighbor filtering on upscaling and downscaling to prevent
+    -- and graphics 
     love.window.setMode(WINDOW_WIDTH, WINDOW_HEIGHT, {
         fullscreen = false,
         resizable = false,
@@ -13,13 +15,13 @@ function love.load()
 end
 
 --[[
-    Called after update by LOVE2D, used to draw anything to the screen
+    Called after update by LOVE2D, used to draw anything to the screen, updated or otherwise.
 ]]
 function love.draw()
     love.graphics.printf(
         'Hello Pong!',          -- text to render
-        0,                      -- starting X (0 since we're going to center)
+        0,                      -- starting X (0 since we're going to center it based on width)
         WINDOW_HEIGHT / 2 - 6,  -- starting Y (halfway down the screen)
-        WINDOW_WIDTH,           -- number of pixels to center within (the entire screen)
+        WINDOW_WIDTH,           -- number of pixels to center within (the entire screen here)
         'center')               -- alignment mode, can be 'center', 'left', or 'right'
 end
